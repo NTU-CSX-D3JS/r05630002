7a80d7da4e3ef97a78e329ed3d9926ffcbaeb57e
iamkailan <31886497+iamkailan@users.noreply.github.com>
2017-11-21 19:11:28 +0800

    Add files via upload

---
+++ jsfunction.js
@@ -0,0 +1,17 @@
+
+function myFunction(k1, k2) {
+    var show = "((" + k1.toString() + "-" + k1.toString() + ")*(" + p1.toString() + "+" + p2.toString() + ")) = ";
+    var ans = ((k1 + k2) * (k1 '+' k2));
+    var str = show + ans.toString();
+    return str
+}
+
+function changeColor(obj) {
+	console.log(obj);
+	var newObj = obj.setAttribute("color", "pink");
+	return newobj;
+}
+
+function d3select(id) {
+	return document.getElementById(id);
+}
